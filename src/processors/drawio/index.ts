3edd63555940429c3acad77b7b3972492a35f16b
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2024-09-28 20:49:39 +0200

    Refactoring

--- src/processors/drawio/index.ts
+++ src/processors/drawio/index.ts
@@ -1,4 +1,4 @@
-import { DrawIoConfig, LogFunc, Scraped } from '../../index.js'
+import { DrawIoConfig, LogFunc, Scraped, ScrapedTable } from '../../index.js'
 import fs from 'fs'
 import { XMLParser } from 'fast-xml-parser'
 import { processTeDrawIo } from './te.js'
@@ -23,4 +23,58 @@ export const processDrawIo = async (
     default:
       throw `${variant} not implemented yet`
   }
+}
+
+export const drawIoHelper = (rawNodes: any[]) => {
+  const getTableMetadata = (node: any, log: LogFunc) => {
+    if (node['@_style'] && node['@_style'].includes('shape=table;')) {
+      const rowIds = rawNodes
+        .filter(
+          (n) =>
+            n['@_style'] &&
+            n['@_style'].includes('shape=tableRow;') &&
+            n['@_parent'] === node['@_id'],
+        )
+        .map((r) => r['@_id'])
+      const values = rowIds.map((rId) =>
+        rawNodes.filter((n) => n['@_parent'] === rId).map((v) => v['@_value']),
+      )
+      const text: string = values[0][0]
+      const isConnected = text.startsWith('.')
+      if (isConnected) {
+        const table: ScrapedTable = {
+          type: 'table',
+          id: node['@_id'],
+          text: text.substring(1),
+          rows: values,
+        }
+        log && log('parsedTable', table)
+        return table
+      }
+    }
+    return undefined
+  }
+
+  const getConnections = (
+    node: any,
+  ): { target: string; text: string | undefined }[] => {
+    const getConnectionText = (id: string) => {
+      const connectionText = rawNodes.find((n) => n['@_parent'] === id)
+      return connectionText ? connectionText['@_value'] : undefined
+    }
+    const connectionNodes = rawNodes.filter(
+      (n: any) =>
+        n['@_source'] === node['@_id'] && !n['@_style']?.includes('dashed'),
+    )
+    return connectionNodes.map((c: any) => ({
+      target: c['@_target'],
+      text: getConnectionText(c['@_id']),
+    }))
+  }
+  const getNext = (node: any) => {
+    const connection = getConnections(node)[0]
+    return connection?.target
+  }
+
+  return { getConnections, getNext, getTableMetadata }
 }
--- src/processors/drawio/te.ts
+++ src/processors/drawio/te.ts
@@ -7,89 +7,20 @@ import {
   ScrapedScript,
   type ScrapedServiceCall,
   ScrapedStart,
-  ScrapedTable,
   ScrapedUserAction,
 } from '../../index.js'
 import { isNodeInside } from '../index.js'
-
-// export const drawIoTeStyles = {
-//   start:
-//     'shape=mxgraph.bpmn.shape;html=1;verticalLabelPosition=bottom;labelBackgroundColor=#ffffff;verticalAlign=top;align=center;perimeter=ellipsePerimeter;outlineConnect=0;outline=standard;symbol=general;fillColor=#99CCFF;',
-//   script:
-//     'shape=ext;rounded=0;html=1;whiteSpace=wrap;glass=0;fillColor=#99CCFF;gradientColor=none;',
-//   message:
-//     'points=[[0.145,0.145,0],[0.5,0,0],[0.855,0.145,0],[1,0.5,0],[0.855,0.855,0],[0.5,1,0],[0.145,0.855,0],[0,0.5,0]];shape=mxgraph.bpmn.event;html=1;verticalLabelPosition=bottom;labelBackgroundColor=#ffffff;verticalAlign=top;align=center;perimeter=ellipsePerimeter;outlineConnect=0;aspect=fixed;outline=end;symbol=message;fillColor=#99CCFF;',
-//   signalSend:
-//     'shape=mxgraph.bpmn.shape;html=1;verticalLabelPosition=bottom;labelBackgroundColor=#ffffff;verticalAlign=top;align=center;perimeter=ellipsePerimeter;outlineConnect=0;outline=end;symbol=signal;rounded=0;glass=0;fillColor=#99CCFF;gradientColor=none;fontColor=none;resizable=0;rotatable=0;strokeColor=#FFFFFF;',
-//   serviceCall:
-//     'shape=ext;rounded=0;html=1;whiteSpace=wrap;glass=0;gradientColor=none;fillColor=#7AB648;',
-//   gateway:
-//     'rhombus;html=1;verticalAlign=top;labelPosition=center;verticalLabelPosition=bottom;align=center;fillColor=#99CCFF;fontSize=12;',
-//   subprocess:
-//     'shape=ext;rounded=0;html=1;whiteSpace=wrap;glass=0;fillColor=#99CCFF;gradientColor=none;',
-// }
-
-// const styles: Record<string, ScrapedNodeTypes> = {
-//   [drawIoTeStyles.start]: 'start',
-//   [drawIoTeStyles.script]: 'script',
-//   [drawIoTeStyles.message]: 'script',
-//   [drawIoTeStyles.signalSend]: 'script',
-//   [drawIoTeStyles.serviceCall]: 'serviceCall',
-//   [drawIoTeStyles.gateway]: 'gateway',
-//   [drawIoTeStyles.subprocess]: 'subprocess',
-// }
+import { drawIoHelper } from './index.js'
 
 export const processTeDrawIo = async (
   data: any,
   log: LogFunc,
 ): Promise<Scraped> => {
   const rawNodes = data.mxfile.diagram.mxGraphModel.root.mxCell
-  const getConnections = (
-    node: any,
-  ): { target: string; text: string | undefined }[] => {
-    const getConnectionText = (id: string) => {
-      const connectionText = rawNodes.find((n) => n['@_parent'] === id)
-      return connectionText ? connectionText['@_value'] : undefined
-    }
-    const connectionNodes = rawNodes.filter(
-      (n: any) =>
-        n['@_source'] === node['@_id'] && !n['@_style']?.includes('dashed'),
-    )
-    return connectionNodes.map((c: any) => ({
-      target: c['@_target'],
-      text: getConnectionText(c['@_id']),
-    }))
-  }
-  const getNext = (node: any) => {
-    const connection = getConnections(node)[0]
-    return connection?.target
-  }
+  const { getNext, getConnections, getTableMetadata } = drawIoHelper(rawNodes)
   const getNodeMetadata = (node: any): ScrapedNode => {
-    if (node['@_style'] && node['@_style'].includes('shape=table;')) {
-      const rowIds = rawNodes
-        .filter(
-          (n) =>
-            n['@_style'] &&
-            n['@_style'].includes('shape=tableRow;') &&
-            n['@_parent'] === node['@_id'],
-        )
-        .map((r) => r['@_id'])
-      const values = rowIds.map((rId) =>
-        rawNodes.filter((n) => n['@_parent'] === rId).map((v) => v['@_value']),
-      )
-      const text: string = values[0][0]
-      const isConnected = text.startsWith('.')
-      if (isConnected) {
-        const table: ScrapedTable = {
-          type: 'table',
-          id: node['@_id'],
-          text: text.substring(1),
-          rows: values,
-        }
-        log && log('parsedTable', table)
-        return table
-      }
-    }
+    const table = getTableMetadata(node, log)
+    if (table) return table
     const type = node['@_type']
     switch (type) {
       case 'message':
