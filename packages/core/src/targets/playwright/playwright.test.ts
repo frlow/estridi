b780a3f50daa702a80eee4b32910d8fab293562a
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2025-02-16 20:26:04 +0100

    Fixed broken tests

--- packages/core/src/targets/playwright/playwright.test.ts
+++ packages/core/src/targets/playwright/playwright.test.ts
@@ -1,7 +1,12 @@
 import { describe, expect, test } from 'vitest'
 import { filterScraped } from '../../common/filter.js'
 import { generatePlaywright } from './index.js'
-import { autoText, loopTestCase, standardTestCase, subprocessLoopTestCase } from '../../test/testCases'
+import {
+  autoText,
+  loopTestCase,
+  standardTestCase,
+  subprocessLoopTestCase,
+} from '../../test/testCases'
 import path from 'node:path'
 import fs from 'node:fs'
 import { Scraped } from '../../scraped'
@@ -9,13 +14,20 @@ import { Scraped } from '../../scraped'
 describe('playwright', () => {
   test('generate playwright test code', async () => {
     const code = await generatePlaywright('main', standardTestCase)
-    const filePath = path.join(__dirname, '../../test', 'playwrightReference.ts')
+    const filePath = path.join(
+      __dirname,
+      '../../test',
+      'playwrightReference.ts',
+    )
     const referenceFileContent = `// @ts-nocheck\n${code}`
     // fs.writeFileSync(filePath, referenceFileContent, 'utf8')
     const reference = fs.readFileSync(filePath, 'utf8')
-    expect(referenceFileContent).toStrictEqual(reference)
+    const normalizeLineEndings = (str, normalized = '\n') =>
+      str.replace(/\r?\n/g, normalized)
+    expect(normalizeLineEndings(referenceFileContent)).toStrictEqual(
+      normalizeLineEndings(reference),
+    )
   })
-
 
   test('loop should not crash', async () => {
     const code = await generatePlaywright('main', loopTestCase)
@@ -28,17 +40,18 @@ describe('playwright', () => {
   })
 
   test('illegal characters in tables', async () => {
-    const scraped: Scraped = [{
-      id: 'startId',
-      type: 'root',
-      next: 'TableSubprocessId',
-      ...autoText('demo')
-    },
+    const scraped: Scraped = [
+      {
+        id: 'startId',
+        type: 'root',
+        next: 'TableSubprocessId',
+        ...autoText('demo'),
+      },
       {
         id: 'TableSubprocessId',
         type: 'subprocess',
         ...autoText('Validate: My Table'),
-        tableKey: 'My Table'
+        tableKey: 'My Table',
       },
       {
         id: 'TableId',
@@ -46,14 +59,19 @@ describe('playwright', () => {
         type: 'table',
         rows: [
           ['My Table', 'Content'],
-          ['Line 1', `/^[a-zåäöüA-ZÅÄÖÜ0-9!&*);\\-/,%:'="$(+. §?]{0,30}$/`]
-        ]
-      }]
+          ['Line 1', `/^[a-zåäöüA-ZÅÄÖÜ0-9!&*);\\-/,%:'="$(+. §?]{0,30}$/`],
+        ],
+      },
+    ]
 
     const filtered = filterScraped(scraped, 'demo')
     const code = await generatePlaywright('main', filtered)
-    const offendingLine = code.split('\n').find(line => line.includes('"Content":')).trim()
-    expect(offendingLine).toEqual(`"Content": "/^[a-zåäöüA-ZÅÄÖÜ0-9!&*);\\\\-/,%:'=\\"$(+. §?]{0,30}$/"`)
+    const offendingLine = code
+      .split('\n')
+      .find((line) => line.includes('"Content":'))
+      .trim()
+    expect(offendingLine).toEqual(
+      `"Content": "/^[a-zåäöüA-ZÅÄÖÜ0-9!&*);\\\\-/,%:'=\\"$(+. §?]{0,30}$/"`,
+    )
   })
 })
-
