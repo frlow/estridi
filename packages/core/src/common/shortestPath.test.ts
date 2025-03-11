1393301f555fee3e29b711935302539ab561ac87
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2025-03-11 22:03:19 +0100

    Made test cases editable in editor

--- packages/core/src/common/shortestPath.test.ts
+++ packages/core/src/common/shortestPath.test.ts
@@ -1,11 +1,11 @@
 import { describe, expect, test } from 'vitest'
 import { findShortestPathToNode } from './shotestPath'
-import { editorTestCases } from '../test/editorTestCases'
+import { getTestCase } from '../test/editorTestCases'
 import { injectVirtualNodes } from './virtualNodes'
 
 describe('find shortest path', () => {
   test("shortest path", async ()=>{
-    const testCase = editorTestCases.shortestpathBase
+    const testCase = await getTestCase("shortestpath-base")
     const scriptNode = testCase.find(n => n.text === 'My node')
     const path = findShortestPathToNode(testCase, scriptNode.id)
     const shortestPathNode = path.find(n=>n.text==="Shortest Path")
@@ -13,7 +13,7 @@ describe('find shortest path', () => {
   })
 
   test('virtual nodes should skip ahead one', async () => {
-    const testCase = editorTestCases.shortestpathVirtual
+    const testCase = await getTestCase("shortestpath-virtual")
     const scriptNode = testCase.find(n => n.text === 'My node')
     const injected = await injectVirtualNodes(testCase)
     const path = findShortestPathToNode(injected, scriptNode.id)
--- packages/core/src/common/virtualNodes.test.ts
+++ packages/core/src/common/virtualNodes.test.ts
@@ -2,126 +2,129 @@ import { describe, expect, test } from 'vitest'
 import { Scraped, ScrapedGateway } from '../scraped'
 import { injectVirtualNodes } from './virtualNodes'
 import { autoText } from './texts'
-import { editorTestCases } from '../test/editorTestCases'
+import { getTestCase } from '../test/editorTestCases'
 
-describe('inject virtual nodes', () => {
-  const gwName = 'is?'
+describe('virtual nodes', () => {
+  describe('inject virtual nodes', () => {
+    const gwName = 'is?'
 
-  test('inject one virtual node', async () => {
-    const scraped: Scraped = [{ type: 'root', id: 'RootId', ...autoText('main'), next: 'GatewayId' },
-      {
-        type: 'gateway', id: 'GatewayId', ...autoText(gwName), variant: 'gateway', options: {
-          'ConnectorId': 'no',
-          'ScriptId': 'yes'
-        }
-      },
-      { type: 'script', variant: 'script', id: 'ScriptId', ...autoText('script'), next: 'ConnectorId' },
-      { type: 'connector', id: 'ConnectorId', ...autoText('') }]
-    const injected = await injectVirtualNodes(scraped)
-    const expected = structuredClone(scraped)
-    expected.find(n => n.type === 'gateway').options = {
-      'GatewayId-Virtual': 'no',
-      'ScriptId': 'yes'
-    }
-    expected.push({
-      'id': 'GatewayId-Virtual',
-      'next': 'ConnectorId',
-      ...autoText('Negative: script'),
-      'type': 'script',
-      'variant': 'virtual' as any
+    test('inject one virtual node', async () => {
+      const scraped: Scraped = [{ type: 'root', id: 'RootId', ...autoText('main'), next: 'GatewayId' },
+        {
+          type: 'gateway', id: 'GatewayId', ...autoText(gwName), variant: 'gateway', options: {
+            'ConnectorId': 'no',
+            'ScriptId': 'yes'
+          }
+        },
+        { type: 'script', variant: 'script', id: 'ScriptId', ...autoText('script'), next: 'ConnectorId' },
+        { type: 'connector', id: 'ConnectorId', ...autoText('') }]
+      const injected = await injectVirtualNodes(scraped)
+      const expected = structuredClone(scraped)
+      expected.find(n => n.type === 'gateway').options = {
+        'GatewayId-Virtual': 'no',
+        'ScriptId': 'yes'
+      }
+      expected.push({
+        'id': 'GatewayId-Virtual',
+        'next': 'ConnectorId',
+        ...autoText('Negative: script'),
+        'type': 'script',
+        'variant': 'virtual' as any
+      })
+      expect(injected).toStrictEqual(expected)
     })
-    expect(injected).toStrictEqual(expected)
-  })
 
-  test('inject two nodes, joined gateways', async () => {
-    const scraped: Scraped = [{ type: 'root', id: 'RootId', ...autoText('main'), next: 'GatewayId' },
-      {
-        type: 'gateway', id: 'GatewayId', ...autoText(gwName), variant: 'gateway', options: {
-          'ConnectorId': 'no',
-          'ScriptId': 'yes'
-        }
-      },
-      { type: 'script', variant: 'script', id: 'ScriptId', ...autoText('script'), next: 'ConnectorId' },
-      { type: 'connector', id: 'ConnectorId', ...autoText(''), next: 'Gateway2Id' },
-      {
-        type: 'gateway', id: 'Gateway2Id', ...autoText(gwName), variant: 'gateway', options: {
-          'Connector2Id': 'no',
-          'Script2Id': 'yes'
-        }
-      },
-      { type: 'script', variant: 'script', id: 'Script2Id', ...autoText('script2'), next: 'Connector2Id' },
-      { type: 'connector', id: 'Connector2Id', ...autoText('') }]
-    const injected = await injectVirtualNodes(scraped)
-    const expected = structuredClone(scraped)
-    ;(expected.find(n => n.id === 'GatewayId') as ScrapedGateway).options = {
-      'GatewayId-Virtual': 'no',
-      'ScriptId': 'yes'
-    }
-    ;(expected.find(n => n.id === 'Gateway2Id') as ScrapedGateway).options = {
-      'Gateway2Id-Virtual': 'no',
-      'Script2Id': 'yes'
-    }
-    expected.push({
-      'id': 'GatewayId-Virtual',
-      'next': 'ConnectorId',
-      ...autoText('Negative: script'),
-      'type': 'script',
-      'variant': 'virtual' as any
+    test('inject two nodes, joined gateways', async () => {
+      const scraped: Scraped = [{ type: 'root', id: 'RootId', ...autoText('main'), next: 'GatewayId' },
+        {
+          type: 'gateway', id: 'GatewayId', ...autoText(gwName), variant: 'gateway', options: {
+            'ConnectorId': 'no',
+            'ScriptId': 'yes'
+          }
+        },
+        { type: 'script', variant: 'script', id: 'ScriptId', ...autoText('script'), next: 'ConnectorId' },
+        { type: 'connector', id: 'ConnectorId', ...autoText(''), next: 'Gateway2Id' },
+        {
+          type: 'gateway', id: 'Gateway2Id', ...autoText(gwName), variant: 'gateway', options: {
+            'Connector2Id': 'no',
+            'Script2Id': 'yes'
+          }
+        },
+        { type: 'script', variant: 'script', id: 'Script2Id', ...autoText('script2'), next: 'Connector2Id' },
+        { type: 'connector', id: 'Connector2Id', ...autoText('') }]
+      const injected = await injectVirtualNodes(scraped)
+      const expected = structuredClone(scraped)
+      ;(expected.find(n => n.id === 'GatewayId') as ScrapedGateway).options = {
+        'GatewayId-Virtual': 'no',
+        'ScriptId': 'yes'
+      }
+      ;(expected.find(n => n.id === 'Gateway2Id') as ScrapedGateway).options = {
+        'Gateway2Id-Virtual': 'no',
+        'Script2Id': 'yes'
+      }
+      expected.push({
+        'id': 'GatewayId-Virtual',
+        'next': 'ConnectorId',
+        ...autoText('Negative: script'),
+        'type': 'script',
+        'variant': 'virtual' as any
+      })
+      expected.push({
+        'id': 'Gateway2Id-Virtual',
+        'next': 'Connector2Id',
+        ...autoText('Negative: script2'),
+        'type': 'script',
+        'variant': 'virtual' as any
+      })
+      expect(injected).toStrictEqual(expected)
     })
-    expected.push({
-      'id': 'Gateway2Id-Virtual',
-      'next': 'Connector2Id',
-      ...autoText('Negative: script2'),
-      'type': 'script',
-      'variant': 'virtual' as any
+  })
+
+  describe('text on virtual nodes', () => {
+    test('base case', async () => {
+      const injected = await injectVirtualNodes(await getTestCase('base'))
+      const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
+      expect(virtualNodes.length).toEqual(1)
+      for (const vn of virtualNodes) {
+        expect(vn.raw).toEqual('Negative: Show Thing')
+      }
     })
-    expect(injected).toStrictEqual(expected)
-  })
-})
 
-describe('text on virtual nodes', () => {
-  test('base case', async () => {
-    const injected = await injectVirtualNodes(editorTestCases.base)
-    const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
-    expect(virtualNodes.length).toEqual(1)
-    for(const vn of virtualNodes){
-      expect(vn.raw).toEqual('Negative: Show Thing')
-    }
-  })
+    test('double case', async () => {
+      const testCase = await getTestCase('double')
+      const injected = await injectVirtualNodes(testCase)
+      const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
+      expect(virtualNodes.length).toEqual(1)
+      for (const vn of virtualNodes) {
+        expect(vn.raw).toEqual('Negative: Show Warning')
+      }
+    })
 
-  test('double case', async () => {
-    const injected = await injectVirtualNodes(editorTestCases.double)
-    const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
-    expect(virtualNodes.length).toEqual(1)
-    for(const vn of virtualNodes){
-      expect(vn.raw).toEqual('Negative: Show Warning')
-    }
-  })
+    test('stair case', async () => {
+      const injected = await injectVirtualNodes(await getTestCase('stair'))
+      const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
+      expect(virtualNodes.length).toEqual(2)
+      for (const vn of virtualNodes) {
+        expect(vn.raw).toEqual('Negative: Show Thing')
+      }
+    })
 
-  test('stair case', async () => {
-    const injected = await injectVirtualNodes(editorTestCases.stair)
-    const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
-    expect(virtualNodes.length).toEqual(2)
-    for(const vn of virtualNodes){
-      expect(vn.raw).toEqual('Negative: Show Thing')
-    }
-  })
+    test('wrong path case', async () => {
+      const injected = await injectVirtualNodes(await getTestCase('wrongpath'))
+      const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
+      expect(virtualNodes.length).toEqual(2)
+      for (const vn of virtualNodes) {
+        expect(vn.raw).toEqual('Negative: Show Thing')
+      }
+    })
 
-  test('wrong path case', async () => {
-    const injected = await injectVirtualNodes(editorTestCases.wrongpath)
-    const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
-    expect(virtualNodes.length).toEqual(2)
-    for(const vn of virtualNodes){
-      expect(vn.raw).toEqual('Negative: Show Thing')
-    }
-  })
-
-  test('closed path case', async () => {
-    const injected = await injectVirtualNodes(editorTestCases.closedpath)
-    const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
-    expect(virtualNodes.length).toEqual(1)
-    for(const vn of virtualNodes){
-      expect(vn.raw).toEqual('Negative: Show Thing')
-    }
+    test('closed path case', async () => {
+      const injected = await injectVirtualNodes(await getTestCase('closedpath'))
+      const virtualNodes = injected.filter((n: any) => n.variant === 'virtual')
+      expect(virtualNodes.length).toEqual(1)
+      for (const vn of virtualNodes) {
+        expect(vn.raw).toEqual('Negative: Show Thing')
+      }
+    })
   })
 })
