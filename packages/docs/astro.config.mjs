c0f583afdc9b83d11e089a7350075528c93018ee
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2025-03-14 21:58:08 +0100

    Revert attempted fix

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -5,6 +5,11 @@ import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    server: `../../../dist/docs/server`,
+    client: `../../../dist/docs/client`
+  },
+
   image: {
     service: passthroughImageService()
   },
