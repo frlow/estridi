659e96e035e779fc770e8f4e32d7344f55e1f751
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2025-03-17 00:11:51 +0100

    Added patch to make docs work on windows

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -3,6 +3,32 @@ import { defineConfig, passthroughImageService } from 'astro/config'
 
 import node from '@astrojs/node'
 
+// @ts-check
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+import fs from 'node:fs'
+import path from 'node:path';
+import url from 'node:url';
+
+const integration = starlight({
+  title: 'estridi',
+  social: {},
+  customCss: [
+    './src/styles/style.css'
+  ],
+  components: {}
+})
+
+integration.hooks["astro:build:ssr"] = options => {
+  const entryFilePath = path.join(url.fileURLToPath(options.manifest.buildServerDir), "entry.mjs")
+  fs.writeFileSync(entryFilePath, `import path from 'path'
+import url from 'url'
+${fs.readFileSync(entryFilePath, 'utf8')
+    .replace(/"client": ".*?",/gs, `"client": url.pathToFileURL(path.join(import.meta.dirname, "../client")).href,`)
+    .replace(/"server": ".*?",/gs, `"server": url.pathToFileURL(import.meta.dirname).href,`)
+  }`, 'utf8')
+}
+
 // https://astro.build/config
 export default defineConfig({
   build: {
@@ -19,14 +45,7 @@ export default defineConfig({
   },
 
   integrations: [
-    starlight({
-      title: 'estridi',
-      social: {},
-      customCss: [
-        './src/styles/style.css'
-      ],
-      components: {}
-    })
+    integration
   ],
 
   output: 'server',
