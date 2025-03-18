aa43638e50004cda0dff87b3c02a2581afedf8d7
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2025-03-18 22:32:49 +0100

    Wip

--- packages/core/src/common/shotestPath.ts
+++ packages/core/src/common/shotestPath.ts
@@ -1,13 +1,24 @@
 import { Scraped, ScrapedNode, ScrapedStart } from '../scraped'
 import { getNodeConnections } from './filter'
 
-const findBlockRoot = (scraped: Scraped, blockPath: any[]): ScrapedStart | undefined => {
+const findBlockRoot = (
+  scraped: Scraped,
+  blockPath: any[],
+): ScrapedStart | undefined => {
   if (blockPath.length === 0) return undefined
-  return scraped.find(n => n.id === blockPath[blockPath.length - 1].link) as ScrapedStart
+  return scraped.find(
+    (n) => n.id === blockPath[blockPath.length - 1].link,
+  ) as ScrapedStart
 }
 
-export const findShortestPathToNode = (scraped: Scraped, nodeId: string, blockPath: any[] = []) => {
-  const root = findBlockRoot(scraped, blockPath) || scraped.find((n: ScrapedStart) => n.type === 'root')
+export const findShortestPathToNode = (
+  scraped: Scraped,
+  nodeId: string,
+  blockPath: any[] = [],
+) => {
+  const root =
+    findBlockRoot(scraped, blockPath) ||
+    scraped.find((n: ScrapedStart) => n.type === 'root')
   if (nodeId === root.id) return []
   let crawled = [[root]]
   let max = 100000
@@ -17,19 +28,23 @@ export const findShortestPathToNode = (scraped: Scraped, nodeId: string, blockPa
       .filter((n) => n.type === 'gateway')
       .map((n) => ({
         name: n.text,
-        value: n.options[path[path.indexOf(n) + 1]?.id]
+        value: n.options[path[path.indexOf(n) + 1]?.id],
       }))
       .reduce(
         (acc, cur) => ({
           ...acc,
-          [cur.name]: [...(acc[cur.name] || []), cur.value]
+          [cur.name]: [...(acc[cur.name] || []), cur.value],
         }),
-        {}
+        {},
       )
     // Filter out gateway values that are linked but have differing values
-    return !Object.values(gatewayValues).some((values: string[]) => {
-      values.some((v, i, arr) => arr.indexOf(v) !== i)
-    })
+    const tested = Object.entries(gatewayValues).map(
+      ([key, value]: [string, string[]]) => {
+        const different = value.filter((v, i, arr) => arr.indexOf(v) !== i)
+        return different || []
+      },
+    )
+    return !tested.some((t) => t.length > 0)
   }
   while (true) {
     const temp: ScrapedNode[][] = []
@@ -49,7 +64,7 @@ export const findShortestPathToNode = (scraped: Scraped, nodeId: string, blockPa
       }
       crawled = temp
       if (max-- === 0) {
-        const problemNode = scraped.find(n => n.id === nodeId)
+        const problemNode = scraped.find((n) => n.id === nodeId)
         debugger
         throw `Unable to find a valid path to node: ${problemNode.id}: ${problemNode.text}
 Check if there are conflicting gateways with the same name in the path. `
