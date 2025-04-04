48e9dbbddcfb27d3f9719a6eef7e97bd10ba483d
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2025-04-04 15:07:25 +0200

    Wip refactor

--- packages/core/src/process/special/special.test.ts
+++ packages/core/src/process/special/special.test.ts
@@ -1,33 +1,43 @@
 import { describe, expect, test } from 'vitest'
 import { getTestCase } from '../../test/editorTestCases'
 import { injectSpecialCases } from './index'
-import { getTestableNodeTree } from '../testableNodeTree'
+import { getTestableNodeTree, NodeTree } from '../testableNodeTree'
+
+const sortTree = (tree: NodeTree)=>{
+  tree.children.sort((a,b)=>a.name.localeCompare(b.name))
+  tree.subprocesses["root"]?.tests?.sort((a,b)=>a.localeCompare(b))
+}
 
 const createTest = (rootName: string) => async () => {
-  const originalKey = `tc-tree-${rootName}`
-  const refKey = `tc-tree-${rootName}-ref`
+  const originalKey = `tc-special-${rootName}`
+  const refKey = `${originalKey}-ref`
   const getTree = async (key: string) => {
     const testCase = await getTestCase(key)
     const injected = injectSpecialCases(testCase)
     return getTestableNodeTree(injected, key)
   }
   const original = await getTree(originalKey)
+
   const referenceJSON = JSON.stringify(await getTree(refKey))
-  const reference = JSON.parse(referenceJSON
-    .replaceAll('-ref-sub', '')
-    .replaceAll('-ref', '')
+  const reference = JSON.parse(
+    referenceJSON.replaceAll('-ref-sub', '').replaceAll('-ref', ''),
   )
+  sortTree(original)
+  sortTree(reference)
   expect(original).toEqual(reference)
 }
 
 describe('inject special transformations', () => {
   describe('virtual nodes', () => {
     test('base', createTest('virtual'))
+    test('double', createTest('virtual-double'))
+    test('stair', createTest('virtual-stair'))
+    test('wrong path', createTest('virtual-wrong-path'))
   })
 
   describe('subprocess', () => {
     test('subprocess with table linked by :', createTest('table'))
 
-    test("subprocess with actions", createTest("subprocess-action"))
+    test('subprocess with actions', createTest('subprocess-action'))
   })
 })
